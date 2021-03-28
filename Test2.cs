29f22cdf7b73d6d017198dd26c6a9ff3d57c4d00
Pimrada641 <pimrada641@hotmail.com>
2021-03-28 15:14:05 +0700

    added file

---
+++ Test2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x = a , y = b;
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                    y = y + b;
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
